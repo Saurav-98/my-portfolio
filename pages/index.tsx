13502b25031ef0bda6556dbf4238802aae2d106c
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-10-02 00:42:32 +0530

    Header Component - 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       {/* Header */}
-
+      <Header />
       {/* Hero */}
 
       {/* About */}
