6e32b5d6be73ffca6b1792418dd223ad245dfcca
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-10-02 01:02:41 +0530

    Header Component - 2 ( Framer Motion)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
     <header className="sticky top-0  p-5 flex items-start justify-between  max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          scale: 0.3,
+          visibility: 0,
+        }}
+        animate={{
+          x: 0,
+          scale: 1,
+          visibility: 1,
+        }}
+        transition={{ duration: 2 }}
+        className="flex flex-row items-center"
+      >
         {/* Social Icons */}
         <SocialIcon
           url="https://youtube.com/sonnysangha"
@@ -28,9 +42,24 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+      </motion.div>
 
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div
+        initial={{
+          x: 500,
+          scale: 0.3,
+          visibility: 0,
+        }}
+        animate={{
+          x: 0,
+          scale: 1,
+          visibility: 1,
+        }}
+        transition={{
+          duration: 2,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -40,7 +69,7 @@ export default function Header({}: Props) {
         <p className="uppercase hidden md:inline-flex text-gray-400 text-sm">
           Get In Touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
